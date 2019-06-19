26ad6d062f1f3aeb4b7169e0f155b2ddf5e7c8d9
Noor ul haq <noor_ul_haq96@hotmail.com>
2019-06-20 01:40:21 +0500

    return static assets from the cache

--- public/sw.js
+++ public/sw.js
@@ -1,41 +1,44 @@
-const cacheName = 'cache-v2';
-const assets = ['/', '/script.js', '/style.css'];
-const excludeCache = ['fonts.googleapis.com'];
+const cacheName = "cache-v2";
+const assets = ["/", "/script.js", "/style.css"];
+const excludeCache = ["fonts.googleapis.com"];
 
-importScripts('/sw-helpers.js');
+importScripts("/sw-helpers.js");
 const helper = new Helper();
 
-self.addEventListener('install', event => {
-	self.skipWaiting();
-	clients.claim();
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  clients.claim();
 
-	event.waitUntil(
-		caches.open(cacheName).then(cache => {
-			return cache.addAll(assets);
-		}),
-	);
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('activate', event => {
-	// delete old cahces
-	event.waitUntil(
-		caches.keys().then(keys => {
-			keys.forEach(key => {
-				if (key !== cacheName)
-					caches
-						.delete(key)
-						.then(isDeleted =>
-							console.log(`cache ${key} deleted ${isDeleted}`),
-						);
-			});
-		}),
-	);
+self.addEventListener("activate", event => {
+  // delete old cahces
+  event.waitUntil(
+    caches.keys().then(keys => {
+      keys.forEach(key => {
+        if (key !== cacheName)
+          caches
+            .delete(key)
+            .then(isDeleted =>
+              console.log(`cache ${key} deleted ${isDeleted}`)
+            );
+      });
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-	// console.log("FetchEvent", event);
-	// const url = new URL(event.request.url);
-	//
-	if (!/https:\/\/fonts\.googleapis\.com/.test(event.request.url))
-		event.respondWith(helper.networkThenCache(event));
+self.addEventListener("fetch", event => {
+  // console.log("FetchEvent", event);
+  const url = new URL(event.request.url);
+  //
+  if (assets.includes(url.pathname)) {
+    event.respondWith(helper.respondFromCache(event));
+  } else if (!/https:\/\/fonts\.googleapis\.com/.test(event.request.url)) {
+    event.respondWith(helper.networkThenCache(event));
+  }
 });
