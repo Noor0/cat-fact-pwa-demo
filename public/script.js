e5c2a8c24a7d745374d0c856ffb6b7ae1beacf79
Noor ul haq <noor_ul_haq96@hotmail.com>
2019-06-19 21:50:48 +0500

    add manifest.json

--- public/script.js
+++ public/script.js
@@ -1,5 +1,18 @@
-fetch("/fact")
-  .then(res => res.json())
-  .then(res => {
-    document.getElementById("fact").innerHTML = res.fact;
-  });
+fetch('/fact')
+	.then(res => res.json())
+	.then(res => {
+		document.getElementById('fact').innerHTML = res.fact;
+	});
+
+let deferredPrompt;
+const addBtn = document.getElementById('aths');
+
+window.addEventListener('beforeinstallprompt', e => {
+	e.preventDefault();
+	deferredPrompt = e;
+	addBtn.style.display = 'block';
+});
+
+addBtn.addEventListener('click', () => {
+	deferredPrompt.prompt();
+});
