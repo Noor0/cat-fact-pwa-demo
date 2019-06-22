a0eb9a196b3fee06d9ce14154cd8f0fb4cb89e33
Noor ul haq <noor_ul_haq96@hotmail.com>
2019-06-23 00:59:17 +0500

    send a response when no connecion is present

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const app = express();
 
 app.use(express.static("public"));
 
+let randomNumber = 0;
+
 app.get("/fact", (req, res) => {
   axios
     .get("https://catfact.ninja/fact")
-    .then(({ data: { fact } }) => res.json({ fact }));
+    .then(({ data: { fact } }) => res.json({ fact }))
+    .catch(err => res.json({ fact: `Error ${randomNumber++}` }));
 });
 
 app.listen(4000);
