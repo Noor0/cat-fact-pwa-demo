0abb83178fb343f215010474efdc29f8818ec449
Noor ul haq <noor_ul_haq96@hotmail.com>
2019-06-23 01:00:14 +0500

    store data in IDB for offline use

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,25 @@ const assets = ["/", "/script.js", "/style.css"];
 const excludeCache = ["fonts.googleapis.com"];
 
 importScripts("/sw-helpers.js");
+importScripts(
+  "https://cdn.jsdelivr.net/npm/idb-keyval@3/dist/idb-keyval-iife.min.js"
+);
 const helper = new Helper();
+
+const openIDB = (db, objectStores) =>
+  new Promise((res, rej) => {
+    let openRequest = indexedDB.open(db, 1);
+    openRequest.onupgradeneeded = function() {
+      objectStores.forEach(os => openRequest.result.createObjectStore(os));
+    };
+    openRequest.onsuccess = function() {
+      res(openRequest.result);
+    };
+
+    openRequest.onerror = function() {
+      rej(openRequest.error);
+    };
+  });
 
 self.addEventListener("install", event => {
   self.skipWaiting();
@@ -15,6 +33,8 @@ self.addEventListener("install", event => {
     })
   );
 });
+
+const offlineStorage = new idbKeyval.Store("offlineStorage", "factsStore");
 
 self.addEventListener("activate", event => {
   // delete old cahces
@@ -38,6 +58,31 @@ self.addEventListener("fetch", event => {
   //
   if (assets.includes(url.pathname)) {
     event.respondWith(helper.respondFromCache(event));
+  } else if (url.pathname.includes("/fact")) {
+    event.respondWith(
+      fetch(event.request)
+        .then(async res => {
+          if (res.ok) {
+            const response = await res.clone().json();
+            const offlineData = await idbKeyval.get("facts", offlineStorage);
+            const updatedArray = [
+              ...(offlineData
+                ? [...offlineData, response].slice(-5)
+                : [response])
+            ];
+            await idbKeyval.set("facts", updatedArray, offlineStorage);
+            return res;
+          }
+        })
+        .catch(async err => {
+          const factsArray = await idbKeyval.get("facts", offlineStorage);
+          const randIndex = Math.floor(Math.random() * (4 - 0 + 1)) + 0;
+          const blob = new Blob([JSON.stringify(factsArray[randIndex])], {
+            type: "application/json"
+          });
+          return new Response(blob, { status: 200, statusText: "ok" });
+        })
+    );
   } else if (!/https:\/\/fonts\.googleapis\.com/.test(event.request.url)) {
     event.respondWith(helper.networkThenCache(event));
   }
